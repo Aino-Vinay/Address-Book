dc50915ecc532136107a93e288fe52c9a784c70b
Aino-Vinay <vinay@ainosoft.com>
2019-01-09 12:09:20 +0530

    Address Book 1st commit

---
+++ address-book/src/Address2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Address2 {
+
+	int id,zip;
+	String name,street,city,country;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+			
+			
+			
+		
+
+	
+}
